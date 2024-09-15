31af6795a2881fdc0a81dad64f2264310eeedbcf
Payam Yektamaram <payamyek@gmail.com>
2024-09-15 14:36:12 -0400

    fix: eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import { FaGithub, FaLinkedinIn, FaMediumM, FaFilePdf } from 'react-icons/fa';
 
 import IconLink from './ui/social-media-linnk';
