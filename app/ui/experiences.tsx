66cf39e4c04a6b1eea61e48d87bc68ee27636500
Payam Yektamaram <payamyek@gmail.com>
2024-09-23 23:57:45 -0400

    refactor: experiences summar

--- app/ui/experiences.tsx
+++ app/ui/experiences.tsx
@@ -10,7 +10,7 @@ const experiences: Array<TimelineItemInterface> = [
       The moment where it all began. A student facing imposter syndrome enters the "real world".
 
       I made an application from scratch! I wanted to call it something cool but we decided on the Lookup Service, well that's what it did anyways.
-      Building a backend was cool but learning React.js was even cooler and the first time I did Frontend!
+      Building a backend was cool but learning React.js was even cooler and the first time I did frontend!
     `,
     company: 'MPAC',
     position: 'left',
@@ -20,9 +20,9 @@ const experiences: Array<TimelineItemInterface> = [
     endDate: new Date(2023, 7),
     jobTitle: 'Systems Developer',
     jobDescription: `
-      After many years of schooling, I returned! I started light with bug fixes but eventually found myself in the heat of a colossal project.
-      I was tasked with being the lead on a tablet application for the country of Ireland. I took some of my best tools; Electron, React, Javascript, and Python.
-      After many hard months of work, I was able to satisfy an entire team of property assessors based in a different country.
+      I returned after many years of schooling! I started light with bug fixes but eventually found myself in the heat of a colossal project.
+      I became the lead on a tablet app project for the country of Ireland. I took some of the best tools; Electron, React, Javascript, and Python to whip up a marvelous gadget.
+      After many hard months of work, we were able to put a smile on our Irish friends.
     `,
     company: 'MPAC',
     position: 'right',
@@ -31,10 +31,10 @@ const experiences: Array<TimelineItemInterface> = [
     startDate: new Date(2023, 7),
     jobTitle: 'Fullstack Developer (I.T Manager)',
     jobDescription: `
-      Manager??? Do not fret. I still code but got recognition for all the hard I had put in.
-      Now it was time for me to deliver a stellar prototype application to help procure Winnipeg as our new client on a multi-year multi-million dollar
-      contract. We did it!! So now im continuously helping update our code-bases to align with needs of our friends in Winnipeg. Had to learn Angular as some
-      of our systems did not undergo the React evolution stage :/
+      Manager??? Do not fret. I still code but got rewarded for all the hard work I had put in.
+      Now I needed to deliver a stellar prototype application to help procure Winnipeg as our new client on a multi-million dollar
+      contract. We did it!! So now I'm continuously modifying our code-bases to align with needs of our friends in Winnipeg. Had to learn Angular as some
+      of our systems did not undergo the React evolution :(
     `,
     company: 'MPAC',
     position: 'left',
