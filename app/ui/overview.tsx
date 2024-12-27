52db2b73d49fa84e9abde0a55fad9cb896e4a491
Payam Yektamaram <payamyek@gmail.com>
2024-12-26 22:55:23 -0500

    refactor: cleanup css

--- app/ui/overview.tsx
+++ app/ui/overview.tsx
@@ -4,23 +4,23 @@ import IconGroup from './icons/icon-group';
 
 export default function Overview() {
   return (
-    <div className='flex min-h-dvh flex-wrap-reverse justify-center md:flex-nowrap md:gap-16'>
-      <div className='flex flex-col justify-center md:gap-y-16'>
-        <div className='flex justify-center gap-x-2 md:flex-col md:gap-y-5'>
-          <div className='text-center text-3xl md:text-8xl'>Payam</div>
-          <div className='text-center text-3xl md:text-8xl'>Yektamaram</div>
+    <div className='flex min-h-dvh flex-wrap-reverse justify-center lg:flex-nowrap lg:gap-16'>
+      <div className='flex flex-col justify-center text-center md:gap-y-16'>
+        <div className='flex justify-center gap-x-2 text-3xl md:flex-col md:gap-y-5 md:text-6xl xl:text-8xl'>
+          <span>Payam</span>
+          <span>Yektamaram</span>
         </div>
         <div className='flex flex-col gap-4 md:gap-5'>
-          <span className='text-md text-center md:text-4xl'>
+          <span className='text-md md:text-4xl'>
             Full-stack Developer @ MPAC
           </span>
-          <span className='text-md text-center italic md:text-3xl'>
+          <span className='text-md italic md:text-3xl'>
             University of Toronto Graduate
           </span>
           <IconGroup />
         </div>
       </div>
-      <div className='flex flex-col justify-end md:justify-center md:gap-y-2'>
+      <div className='flex shrink-0 flex-col justify-end md:justify-center md:gap-y-2'>
         <Image
           src='/payam.jpg'
           priority
@@ -35,7 +35,7 @@ export default function Overview() {
           width={300}
           height={400}
           alt='A picture of Payam Yektamaram steering a sail-boat in Squamish, British Columbia.'
-          className='rounded-xl shadow-lg shadow-teal-600/30 duration-200 ease-linear hover:scale-105 md:hidden'
+          className='rounded-xl shadow-lg shadow-teal-600/30 md:hidden'
         />
         <span className='text-md hidden w-full text-center italic md:inline-block'>
           A happy Payam in the wild ...
