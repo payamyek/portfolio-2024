1ca9c8f14592578388fc9c729dcf565990553a11
Payam Yektamaram <payamyek@gmail.com>
2025-05-28 14:50:39 -0400

    feat: prepare for version upgrade

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from 'tailwindcss';
 
-import daisyui from 'daisyui';
-
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -16,24 +14,6 @@ const config: Config = {
       },
     },
   },
-  daisyui: {
-    themes: [
-      {
-        rustic: {
-          primary: '#D9D9D9',
-          secondary: '#CBC3e3',
-          accent: '#D0CAE0',
-          neutral: '#ffe4e6',
-          'base-100': '#2B2929',
-          info: '#d8b4fe',
-          success: '#f0abfc',
-          warning: '#c4b5fd',
-          error: '#ec4899',
-        },
-      },
-    ],
-  },
-  plugins: [daisyui],
 };
 
 export default config;
